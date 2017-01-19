875c5eade230182249a156b73b4a3b685763ae60
Cody Brubaker <cody.brubaker>
2017-01-19 11:30:46 -0800

    add piping and allow you to filter by completeness

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,8 +4,17 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+      <option value="allTasks">All Tasks</option>
+      <option value="completedTasks">Completed Tasks</option>
+      <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+    </select>
+
   <ul>
-    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}}  <button (click)="editButtonHasBeenClicked(currentTask)">Edit</button></li>
+    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}}
+    <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+    <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+    <button (click)="editButtonHasBeenClicked(currentTask)">Edit</button></li>
   </ul>
   `
 })
@@ -26,4 +35,13 @@ export class TaskListComponent {
     }
   }
 
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
+  }
+
+  filterByCompleteness: string = "incompleteTasks";
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
 }
